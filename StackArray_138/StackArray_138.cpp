9d792c096632c7e4cebb499233df2ab346e82a56
NilaAyu1611 <nilaayucw@gmail.com>
2023-06-15 18:38:13 +0700

    method pop dengan implementasinya pada step 1  (a, b)dimana jika di dalam array tersebut kosng atau diindeks -1 itu berarti data masih kosong. maka tidak bisa di pop, dan akan dikembalikan

--- StackArray_138/StackArray_138.cpp
+++ StackArray_138/StackArray_138.cpp
@@ -11,18 +11,31 @@ public:
 	StackArray() {
 		top = -1;
 	}
-void push() { // mengembalikan nilai Element(void)... (gara2 ada parametrenya)jadi gunain return tadi... tapi kalo semisal bukan void, tapi string itu kalo nggak ada returnnya maka nanti eror. 
-	cout << "\nEnter a element: ";
-	string element;
-	getline(cin, element);
+	void push() { // mengembalikan nilai Element(void)... (gara2 ada parametrenya)jadi gunain return tadi... tapi kalo semisal bukan void, tapi string itu kalo nggak ada returnnya maka nanti eror. 
+		cout << "\nEnter a element: ";
+		string element;
+		getline(cin, element);
 
-	if (top == 4) {		// Step 1
-		cout << "Number of data exceeds the limit." << endl;
-		return;
+		if (top == 4) {		// Step 1
+			cout << "Number of data exceeds the limit." << endl;
+			return;
+		}
+		top++;		//Step 2
+		stack_array[top] = element;		//Step 3
+		cout << endl;
+		cout << element << " ditambahakan(pushed)" << endl;
+
 	}
-	top++;		//Step 2
-	stack_array[top] = element;		//Step 3
-	cout << endl;
-	cout << element << " ditambahakan(pushed)" << endl;
 
-}
+	void pop() {
+		if (empty()) {			// Step 1
+			cout << "\nStack is empty. Cannot pop." << endl;		// 1.a
+			return;												// 1.b
+		}
+
+		
+
+
+	}
+
+};
